e55b5a30ab4fd9bbd001fd019c2e8064ceaf864e
Oscar <oscar@Oscars-iMac.local>
2019-02-22 00:24:42 -0800

    correcciones en organizacion de codigo

--- 4queens.playground/Contents.swift
+++ 4queens.playground/Contents.swift
@@ -1,21 +1,15 @@
 import UIKit
 
-var str = "Hello, playground"
-
-
 //Por Oscar Beltran
 
 var tablero = Array(repeating: Array(repeating: 0 , count: 4), count: 4)
-//print(tablero[0][0])
 
 var puntoMarcado:Bool = false
 var colum:Int = 0
 var posiciones = Array(repeating: 0, count: 4)
-//tablero[0][1] = 1
-//print(tablero[3][3])
 
-
-
+var indexFila:Int = 0
+var posicion1raFila:Int = 0
 
 func columnOcupada (_ colum1:Int) -> Bool{
     var lleno:Bool = false
@@ -29,14 +23,6 @@ func columnOcupada (_ colum1:Int) -> Bool{
 
 func choca (_ fila2:Int, _ column2:Int) -> Bool{
     var chocanAmbos:Bool = false
-    var dif:Int = fila2 - column2
-    
-    var filasMovidas:Int = 0
-    var columnasMovidas:Int = 0
-    
-    if dif < 0 { columnasMovidas += column2 }
-    else if dif > 0 { filasMovidas += fila2 }
-    
     
     //superior derecha
     for indexZ1 in 1...3{
@@ -75,23 +61,15 @@ func limpiarLineas () -> Void{
         } }
 }
 
-
-
-var indexFila:Int = 0
-var posicion1raFila:Int = 0
-
-
+//Aqui comienza la funcion
 while indexFila < 4 {
     print("Fila  \(indexFila)")
-    //print("///////////////////////////////////////////////")
     puntoMarcado = false
-    //print("Columna actual  \(colum)")
     while puntoMarcado == false {
         
         print("------------------------------------")
         print("Columna  \(colum)")
         
-            
             if columnOcupada(colum) == false {//te da un error de out of index (el index daba 4)
                 
                 if choca(indexFila, colum) == false {
@@ -99,7 +77,6 @@ while indexFila < 4 {
                     tablero[indexFila][colum] = 1
                     posiciones[indexFila] = colum
                     puntoMarcado = true
-                    //colum = 4
                 }
             }
             
@@ -116,7 +93,7 @@ while indexFila < 4 {
                 
             } else if indexFila == 0 {
                 posicion1raFila = colum
-                //print("priemra fila se ubica en  \(posicion1raFila)")
+                print("primera fila se ubica en  \(posicion1raFila)")
                 
         }
         
@@ -129,7 +106,7 @@ while indexFila < 4 {
     }
     indexFila += 1
     colum = 0
-    print("Columna ahora vale  \(colum)")
+    //print("Columna ahora vale  \(colum)")
     print("////////////////////////////////////////////////")
     
 }
